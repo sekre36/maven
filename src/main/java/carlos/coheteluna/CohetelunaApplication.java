1d4acba0690fffdcb3b133a1895b0f97696c8e40
sekre36 <sekre36@gmail.com>
2024-06-05 10:01:44 +0200

    Add files via upload

---
+++ src/main/java/carlos/coheteluna/CohetelunaApplication.java
@@ -0,0 +1,13 @@
+package carlos.coheteluna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CohetelunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CohetelunaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/carlos/coheteluna/CohetelunaApplicationTests.java
@@ -0,0 +1,13 @@
+package carlos.coheteluna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CohetelunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
